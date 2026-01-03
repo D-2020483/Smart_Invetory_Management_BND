beb92c1668a70b2fdc1da9de069134309ec81c1e
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2026-01-04 01:54:37 +0530

    changes

--- server.js
+++ server.js
@@ -1,63 +1,31 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import inventoryRoutes from './routes/inventoryRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import connectDB from "./config/db.js";
-import inventoryRoutes from "./routes/inventoryRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-
-// Load environment variables
+import dotenv from 'dotenv';
 dotenv.config();
 
-// Fix __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Connect to MongoDB
+
 connectDB();
 
 const app = express();
 
+app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 
-const allowedOrigins = [
-  "https://inventory-management-bnd-dinithi.netlify.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (Postman, mobile apps)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
-
-// Handle preflight requests
-app.options("*", cors());
+app.use(express.json()); 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/inventory", inventoryRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-app.get("/", (req, res) => {
-  res.send("API is running successfully 🚀");
-});
+app.use('/api/auth', authRoutes);
+app.use('/api/inventory', inventoryRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
